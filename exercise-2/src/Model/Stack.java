61ce49396bb9dbb044bce4eb90f46b3bc699cb58
Fernanda González <armi3@protonmail.ch>
2019-01-27 22:56:47 -0600

    Stack class updated.

--- exercise-2/src/Model/Stack.java
+++ exercise-2/src/Model/Stack.java
@@ -1,33 +1,87 @@
 package Model;
+import java.util.Arrays;
 
 public class Stack implements Container {
 	
-	public String[] objects; 
-	public boolean isEmpty;
+	private String[] containedObjects; 
+	private int dynamicSize;
 
-	public boolean isEmpty() {
-		return isEmpty;
+	/**
+	 * @param containedObjects
+	 * @param dynamicSize
+	 */
+	public Stack(String[] containedObjects, int dynamicSize) {
+		super();
+		this.containedObjects = containedObjects;
+		this.dynamicSize = dynamicSize;
+	}
+	
+	public Stack() {
+		//super();
+		this.dynamicSize = 0;
+		this.containedObjects = new String[dynamicSize];	
 	}
 
-	public void setEmpty(boolean isEmpty) {
-		this.isEmpty = isEmpty;
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Stack [containedObjects=" + Arrays.toString(containedObjects) + ", dynamicSize=" + dynamicSize + "]";
+	}
+
+	/**
+	 * @return the containedObjects
+	 */
+	public String[] getContainedObjects() {
+		String[] copy = new String[this.containedObjects.length];
+		System.arraycopy(this.containedObjects, 0, copy, 0, copy.length);
+		return copy;
+	}
+
+	/**
+	 * @param containedObjects the containedObjects to set
+	 */
+	public void setContainedObjects(String[] containedObjects) {
+		this.containedObjects = new String[containedObjects.length];
+		System.arraycopy(containedObjects, 0, this.containedObjects, 0, containedObjects.length);
+	}
+
+	/**
+	 * @return the dynamicSize
+	 */
+	public int getDynamicSize() {
+		return dynamicSize;
+	}
+
+	/**
+	 * @param dynamicSize the dynamicSize to set
+	 */
+	public void setDynamicSize(int dynamicSize) {
+		this.dynamicSize = dynamicSize;
 	}
 
 	@Override
 	public void in(String object) {
 		// TODO Auto-generated method stub
-		objects[objects.length] = object;
+		setDynamicSize(getContainedObjects().length + 1);
+		String[] updatedStack = new String[getDynamicSize()];
+		System.arraycopy(getContainedObjects(), 0, updatedStack, 0, getContainedObjects().length);
+		updatedStack[getDynamicSize()-1] = object;
+		setContainedObjects(updatedStack);
 	}
 
 	@Override
 	public String out() {
 		// TODO Auto-generated method stub
 		String popped ="";
-		if (isEmpty=false) {
-			popped = objects[objects.length-1];
-			for (int i=objects.length; i>1; i++) {
-				
-			}
+		if (getDynamicSize() > 0) {
+			setDynamicSize(getContainedObjects().length - 1);
+			popped = getContainedObjects()[getDynamicSize()];
+			
+			String[] updatedStack = new String[getDynamicSize()];
+			System.arraycopy(getContainedObjects(), 0, updatedStack, 0, getDynamicSize());
+			setContainedObjects(updatedStack);
 		}
 		return popped;
 	}
@@ -35,14 +89,18 @@ public class Stack implements Container {
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		objects = null;
-		isEmpty = true;
+		setDynamicSize(0);
+		setContainedObjects(new String[0]);
 	}
 
 	@Override
 	public String look() {
 		// TODO Auto-generated method stub
-		return null;
+		String lifo = "";
+		if (getDynamicSize() > 0) {
+			lifo = getContainedObjects()[getDynamicSize() - 1];
+		}
+		return lifo;
 	}
-
+	
 }

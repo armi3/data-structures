5df567246b62b1747731237ca3277e322ba54b76
Fernanda González <armi3@protonmail.ch>
2019-01-30 18:54:09 -0600

    Exercise 3 created.

---
+++ exercise-3/src/Model/Account.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Model;
+
+/**
+ * @author baroness
+ *
+ */
+
+public class Account {
+	private long creditBalance; // positive
+	private long debitBalance;
+	private long balance;
+	private String currency;
+	private long[] creditHistory;
+	private long[] debitHistory;
+
+}

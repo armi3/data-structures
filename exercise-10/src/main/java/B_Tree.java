e592d2b945c25f2d32deb6b9ea06054ff61e3ed2
armi3 <fernandagonzalez@ufm.edu>
2019-03-19 12:21:53 -0600

    :ohno:

--- exercise-10/src/main/java/B_Tree.java
+++ exercise-10/src/main/java/B_Tree.java
@@ -25,8 +25,58 @@ public class B_Tree {
     }
 
     public void insert(String key){
-        Node nodeToPlace = searchNodeToPlace(key, root);
-        if (){
+        Subnode subnode = new Subnode(key);
+        if (root==null){
+            Subnode[] subnodes = new Subnode[1];
+            subnodes[0] = subnode;
+            this.root = new Node(null, subnodes, true);
+
+        }
+        else {
+            Node nodeToPlace = searchNodeToPlace(key, root);
+            int indexToPlace = searchIndexToPlace(key, nodeToPlace);
+            nodeToPlace.addSubnode(subnode, indexToPlace);
+
+            if(!(nodeToPlace.getChildren().length < degree)){
+                // find median
+                int medianIndex;
+                if((this.degree&1)==0){ // even degree
+                    medianIndex = (this.degree/2)-1;
+                } else{ // odd degree
+                    medianIndex = (this.degree/2);
+                }
+                // push median to parent
+                String poppedKey = nodeToPlace.popSubnode(medianIndex);
+                pushToParent(poppedKey, nodeToPlace.getParent());
+            }
+        }
+    }
+
+    public void pushToParent(String key, Node node){
+        Subnode subnode = new Subnode(key);
+        if(node==null){
+            Subnode[] subnodes = new Subnode[1];
+            subnodes[0] = subnode;
+            Node newRoot = new Node(null, subnodes, false);
+            this.root = newRoot;
+        }
+        else {
+            int indexToPlace = searchIndexToPlace(key, node);
+            node.addSubnode(subnode, indexToPlace);
+
+            if(!(node.getChildren().length < degree)){
+                // find median
+                int medianIndex;
+                if((this.degree&1)==0){ // even degree
+                    medianIndex = (this.degree/2)-1;
+                } else{ // odd degree
+                    medianIndex = (this.degree/2);
+                }
+                // push median to parent
+                String poppedKey = node.popSubnode(medianIndex);
+                pushToParent(poppedKey, node.getParent());
+            }
+
 
         }
 
--- exercise-10/src/main/java/Node.java
+++ exercise-10/src/main/java/Node.java
@@ -1,10 +1,12 @@
 
 public class Node {
 
+    private Node parent;
     private Subnode[] children;
     private boolean isLeaf;
 
-    public Node(Subnode[] children, boolean isLeaf) {
+    public Node(Node parent, Subnode[] children, boolean isLeaf) {
+        this.parent = parent;
         this.children = children;
         this.isLeaf = isLeaf;
     }
@@ -24,4 +26,21 @@ public class Node {
     public void setLeaf(boolean leaf) {
         isLeaf = leaf;
     }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public void addSubnode(Subnode subnode, int indexToPlace){
+
+    }
+
+    public String popSubnode(int indexToPop){
+
+    }
+
 }
